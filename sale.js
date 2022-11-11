c549f43ed3bdba76d6cdc760289a31fb9a91c3c9
aman7github <amanmaurya9044@gmail.com>
2022-11-11 12:36:58 +0530

    k

--- sale.js
+++ sale.js
@@ -4,8 +4,8 @@
 let data = [
     {
         id: 1,
-        image:"https://cdn.modesens.com/product/54111085_1?w=400",
-        icon: '<i class="fa fa-thin fa-heart"  ></i>',
+        image:["https://cdn.modesens.com/product/54111085_1?w=400","https://cdn.modesens.com/media/144038500","https://cdn.modesens.com/media/144038501"],
+        // icon: '<i class="fa fa-thin fa-heart"  ></i>',
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -13,8 +13,8 @@ let data = [
     },
     {
         id: 2,
-        image:"https://cdn.modesens.com/product/23141173_32?w=400",
-        icon: '<i class="fa fa-thin fa-heart"  ></i>',
+        image:["https://cdn.modesens.com/product/23141173_32?w=400","https://cdn.modesens.com/media/100438917","https://cdn.modesens.com/media/100438919"],
+        // icon: '<i class="fa fa-thin fa-heart"  ></i>',
         p1: "MM6 MAISON MARGIELA",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (65% OFF) - $1339",
@@ -23,7 +23,7 @@ let data = [
 
     {
         id: 3,
-        image:"https://cdn.modesens.com/product/43276088_3?w=400",
+        image:["https://cdn.modesens.com/product/44822401_1?w=400","https://cdn.modesens.com/media/152045147","https://cdn.modesens.com/media/132613532"],
         p1: "ZADIG & VOLTAIRE",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -31,7 +31,7 @@ let data = [
     },
     {
         id: 4,
-        image:"https://cdn.modesens.com/media/113969129",
+        image:["https://cdn.modesens.com/product/47908781_1?w=400","https://cdn.modesens.com/media/135499063","https://cdn.modesens.com/media/145671761"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1439",
@@ -39,7 +39,7 @@ let data = [
     },
     {
         id: 5,
-        image:"https://cdn.modesens.com/product/49258903_1?w=400",
+        image:["https://cdn.modesens.com/product/40692626?w=400","https://cdn.modesens.com/media/152658263"],
         p1: "ZADIG & VOLTAIRE",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1754",
@@ -47,7 +47,7 @@ let data = [
     },
     {
         id: 6,
-        image:"https://cdn.modesens.com/product/33906412_2?w=400",
+        image:["https://cdn.modesens.com/product/47184764_2?w=400","https://modesens.com/product/ted-baker-dorras-crewneck-boucle-sweater-natural-47184764/","https://cdn.modesens.com/media/135061219"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -55,7 +55,7 @@ let data = [
     },
     {
         id: 7,
-        image:"https://cdn.modesens.com/availability/54871557?w=400",
+        image:["https://cdn.modesens.com/product/47955731_2?w=400","https://cdn.modesens.com/media/135567660","https://cdn.modesens.com/media/135687205"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1539",
@@ -63,7 +63,7 @@ let data = [
     },
     {
         id: 8,
-        image:"https://cdn.modesens.com/product/43276085_1?w=400",
+        image:["https://cdn.modesens.com/availability/58221846?w=400","https://cdn-images.farfetch-contents.com/18/68/11/44/18681144_41839830_1000.jpg","https://cdn-images.farfetch-contents.com/18/68/11/44/18681144_41959948_1000.jpg"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1869",
@@ -72,7 +72,7 @@ let data = [
 
     {
         id: 9,
-        image:"https://cdn.modesens.com/availability/53218924?w=400",
+        image:["https://cdn.modesens.com/product/56147814_1?w=400","https://cdn.modesens.com/media/145903495","https://cdn.modesens.com/media/145903496"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1379",
@@ -80,7 +80,7 @@ let data = [
     },
     {
         id: 10,
-        image:"https://cdn.modesens.com/product/35824732_2?w=400",
+        image:["https://cdn.modesens.com/product/49600155_1?w=400","https://cdn.modesens.com/media/137241705","https://cdn.modesens.com/media/137241706"],
         p1: "TED BEAKER",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1989",
@@ -88,7 +88,7 @@ let data = [
     },
     {
         id: 11,
-        image:"https://cdn.modesens.com/product/42858981_1?w=400",
+        image:["https://cdn.modesens.com/product/45866749_2?w=400","https://www.net-a-porter.com/variants/images/36856120585214900/in/w1000.jpg","https://www.net-a-porter.com/variants/images/36856120585214900/ou/w1000.jpg"],
         p1: "TED BEAKER",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -96,7 +96,7 @@ let data = [
     },
     {
         id: 12,
-        image:"https://cdn.modesens.com/product/43559321?w=400",
+        image:["https://cdn.modesens.com/product/46412061_6?w=400","https://cdn.modesens.com/media/134144512","https://cdn.modesens.com/media/134629383"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -104,7 +104,7 @@ let data = [
     },
     {
         id: 13,
-        image:"https://cdn.modesens.com/product/41496085?w=400",
+        image:["https://cdn.modesens.com/product/53382749_1?w=400","https://cdn.modesens.com/media/143060693","https://cdn.modesens.com/media/143060694"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -112,7 +112,7 @@ let data = [
     },
     {
         id: 14,
-        image:"https://cdn.modesens.com/product/36407978_3?w=400",
+        image:["https://cdn.modesens.com/availability/45883849?w=400","https://cdn.modesens.com/media/113428424"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -120,7 +120,7 @@ let data = [
     },
     {
         id: 15,
-        image:"https://cdn.modesens.com/product/41586467_3?w=400",
+        image:["https://cdn.modesens.com/product/49131532_2?w=400","https://cdn.modesens.com/media/136727277","https://cdn.modesens.com/media/136727278"],
         p1: "TED BEAKER",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -128,7 +128,7 @@ let data = [
     },
     {
         id: 16,
-        image:"https://cdn.modesens.com/product/40847764_2?w=400",
+        image:["https://cdn.modesens.com/product/49688111_1?w=400","https://cdn.modesens.com/media/137346685","https://cdn.modesens.com/media/137346686"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -136,7 +136,7 @@ let data = [
     },
     {
         id: 17,
-        image:"https://cdn.modesens.com/availability/47144239?w=400",
+        image:["https://cdn.modesens.com/availability/49853523_1?w=400","https://cdn.modesens.com/media/133669369","https://cdn.modesens.com/media/134507889"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -144,7 +144,7 @@ let data = [
     },
     {
         id: 18,
-        image:"https://cdn.modesens.com/availability/56711664?w=400",
+        image:["https://cdn.modesens.com/product/20479938_5?w=400","https://cdn.modesens.com/media/85824113","https://cdn.modesens.com/media/85824114"],
         p1: "ZADIG & VOLTAIRE",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -152,7 +152,7 @@ let data = [
     },
     {
         id: 19,
-        image:"https://cdn.modesens.com/product/40248604_1?w=400",
+        image:["https://cdn.modesens.com/product/35826079_18?w=400","https://cdn.modesens.com/media/116831111","https://cdn.modesens.com/media/122012161"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -160,7 +160,7 @@ let data = [
     },
     {
         id: 20,
-        image:"https://cdn.modesens.com/product/37760178_1?w=400",
+        image:["https://cdn.modesens.com/availability/57115433?w=400","https://cdn.modesens.com/media/147344137","https://cdn.modesens.com/media/147344131"],
         p1: "ZADIG & VOLTAIRE",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -168,7 +168,7 @@ let data = [
     },
     {
         id: 21,
-        image:"https://cdn.modesens.com/product/32776939_1?w=400",
+        image:["https://cdn.modesens.com/product/22013195_76?w=400","https://cdn.modesens.com/media/75190227","https://cdn.modesens.com/media/75190228"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -176,7 +176,7 @@ let data = [
     },
     {
         id: 22,
-        image:"https://cdn.modesens.com/product/39063384_1?w=400",
+        image:["https://cdn.modesens.com/product/46476421_5?w=400","https://cdn.modesens.com/media/147247788","https://cdn.modesens.com/media/145622547"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -184,7 +184,7 @@ let data = [
     },
     {
         id: 23,
-        image:"https://cdn.modesens.com/product/42653173_1?w=400",
+        image:["https://cdn.modesens.com/product/42048002_1?w=400","https://cdn.modesens.com/media/129922707","https://cdn.modesens.com/media/129922708"],
         p1: "ZADIG & VOLTAIRE",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -192,7 +192,7 @@ let data = [
     },
     {
         id: 24,
-        image:"https://cdn.modesens.com/product/32031768_9?w=400",
+        image:["https://cdn.modesens.com/product/49131340_1?w=400","https://cdn.modesens.com/media/142207157","https://cdn.modesens.com/media/142207158"],
         p1: "TED BEAKER",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -200,7 +200,7 @@ let data = [
     },
     {
         id: 25,
-        image:"https://cdn.modesens.com/product/61044382_2?w=400",
+        image:["https://cdn.modesens.com/availability/32917422?w=400","https://cdn.modesens.com/media/103347189","https://cdn.modesens.com/media/103347190"],
         p1: "TED BEAKER",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -208,7 +208,7 @@ let data = [
     },
     {
         id: 26,
-        image:"https://cdn.modesens.com/availability/53747096?w=400",
+        image:["https://cdn.modesens.com/product/17519178_3?w=400","https://www.yoox.com/images/items/11/11821436xq_12_d.jpg","https://www.yoox.com/images/items/11/11821436xq_12_e.jpg"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -216,7 +216,7 @@ let data = [
     },
     {
         id: 27,
-        image:"https://cdn.modesens.com/availability/51287311?w=400",
+        image:["https://cdn.modesens.com/product/28662990_6?w=400","https://cdn.modesens.com/media/109738863","https://cdn.modesens.com/media/109738864"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -224,7 +224,7 @@ let data = [
     },
     {
         id: 28,
-        image:"https://cdn.modesens.com/availability/56711726?w=400",
+        image:["https://cdn.modesens.com/product/12867070_1?w=400","https://cdn.modesens.com/media/66637314"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -232,7 +232,7 @@ let data = [
     },
     {
         id: 29,
-        image:"https://cdn.modesens.com/availability/56045813?w=400",
+        image:["https://cdn.modesens.com/availability/54916901?w=400","https://cdn.modesens.com/media/145768400","https://cdn.modesens.com/media/145768403"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -240,7 +240,7 @@ let data = [
     },
     {
         id: 30,
-        image:"https://cdn.modesens.com/product/49258903_1?w=400",
+        image:["https://cdn.modesens.com/product/20600685_98?w=400","https://cdn.modesens.com/media/95371465","https://cdn.modesens.com/media/95371467"],
         p1: "ETRO",
         p2: "Black Nylon Floral Bomber Jacket",
         p3: "$462 (60% OFF) - $1739",
@@ -258,15 +258,24 @@ const append =(data)=>{
       
       let card = document.createElement("div")
       card.className = "card"
-    
+      card.addEventListener("click", function(){
+        selectedproduct(el)
+      })
       let img = document.createElement("img")
       img.className = "image"
       img.id= i
       img.src = el.image
+      img.addEventListener("mouseenter",function(){
+        changeimage(i)
+      })
+      img.addEventListener("mouseout", function(){
+        remfun(i,img) 
+      })
+     
 
       let icon = document.createElement("icon")
       icon.className = "icon"
-      icon.innerHTML = el.icon
+      icon.innerHTML = '<i class="fa fa-thin fa-heart"  ></i>'
 
       icon.addEventListener("click", function(){
         iconfun(i)
@@ -296,6 +305,9 @@ const append =(data)=>{
 
 
 let f;
+
+let totalbtn;
+
 const showbtn=(image_per_page)=>{
     let arr2 =[]
     for(let i=0;i<image_per_page;i++){
@@ -304,20 +316,29 @@ const showbtn=(image_per_page)=>{
     }
     append(arr2)
 
-    let totalbtn = Math.ceil(data.length/image_per_page)
-    for(let i=1;i<=totalbtn;i++){
+     totalbtn = Math.ceil(data.length/image_per_page)
+
+
+    let btn4 = document.createElement("button")
+    btn4.innerText = "<"
+    btn4.addEventListener("click", function(){
+        reversebtn(image_per_page,totalbtn)
+    })
+    document.querySelector(".pagination").append(btn4)
+
+    for(let h=1;h<=totalbtn;h++){
     let btn = document.createElement("button")
-    btn.id = i;
-    btn.innerText = i;
+    btn.id = h;
+    btn.innerText = h;
     document.querySelector(".pagination").append(btn)
         btn.onclick=()=>{
-            console.log(i)
-            num(i)
+            
+            num(h,image_per_page)
              document.querySelector("#product").innerHTML = " "
-             let adddata = image_per_page*i
+             let adddata = image_per_page*h
           
              let arr = []
-             for(let s=image_per_page*(i-1);s<adddata;s++){
+             for(let s=image_per_page*(h-1);s<adddata;s++){
               
              arr.push(data[s])
       
@@ -340,11 +361,15 @@ const showbtn=(image_per_page)=>{
 showbtn(12)
 
 let m = 0;
-const num=(i)=>{
-    m=i
+ num=(h)=>{
+    m=h
 }
+let count;
 const forwordbtn = (image_per_page,totalbtn)=>{
-
+count++
+if(count>totalbtn){
+    count=1
+}
    if(m==totalbtn){
     m=0
    }
@@ -359,8 +384,62 @@ const forwordbtn = (image_per_page,totalbtn)=>{
 
 }
 
-const iconfun=(i)=>{
+let n = totalbtn;
+ num = (h)=>{
+ n=h
+}
+const reversebtn = (image_per_page)=>{
+    n--
+    if(n<=1){
+        n=1
+       }
+     let adddata = image_per_page*n
+     let arr4= []
+     for(let s=image_per_page*(n-1);s<adddata;s++){
+     arr4.push(data[s])
+     }
+     document.querySelector("#product").innerHTML = " "
+     append(arr4)
  
-  console.log(i)
+ }
+
+const iconfun=(i)=>{
+    let newicon = document.querySelectorAll(".icon")
+   newicon[i].classList.add("changeicon")
+
+}
+let ff;
+ num = (h)=>{
+ ff=h
+}
+
+const changeimage = (i) =>{
+    newimage = document.querySelectorAll(".image")
+  if(ff==undefined){
+    ff=1
+  }
+    let v = 12*(ff-1)+i
+  
+    let j=1
+    let imgarr = data[v].image
+//  console.log(i)
+   setInterval(function(){
+    if(j==imgarr.length){
+        j=0
+    }
+    newimage[i].src = imgarr[j]
+     j++  
+   },1000)
+}
+const remfun=(i,img)=>{
+    img.removeEventListener("mouseenter",function(){
+        changeimage(i), {passive: true}
+      })
+}
+
+const selectedproduct = (el) =>{
+let arr = []
+arr.push(el)
+localStorage.setItem("selectpro", JSON.stringify(arr))
 
 }
